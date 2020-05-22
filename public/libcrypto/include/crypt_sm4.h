92153e38b41c2ac571db1eae2eb8112edeaf98d8
陈文静(chenwenjing) <chenwj@sense.com.cn>
2020-05-22 18:19:51 +0800

    [MOD] add sm4

---
+++ public/libcrypto/include/crypt_sm4.h
@@ -0,0 +1,15 @@
+#ifndef __CRYPT_SM4_H
+#define __CRYPT_SM4_H
+
+#include <stdint.h>
+
+typedef struct _sm4_key_st {
+	uint32_t round_key[32];
+} sm4_key_st;
+
+int qinn_sm4_init_key(unsigned char *key, int key_len, sm4_key_st *sm4_key);
+
+int qinn_sm4_block_encrypt(sm4_key_st *sm4_key, unsigned char *in,
+	int inlen, unsigned char *out);
+
+#endif
